0b698f2156feeef8fb0af0092f83b24e0bb06318
omerreisss <comertomerizzet@gmail.com>
2025-04-28 18:12:32 +0300

    Update bannedİp.js

--- models/bannedİp.js
+++ models/bannedİp.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const bannedIpSchema = new mongoose.Schema({
+  ip: { type: String, required: true },
+  bannedAt: { type: Date, default: Date.now }
+});
+
+const BannedIp = mongoose.model('BannedIp', bannedIpSchema);
+
+module.exports = BannedIp;
